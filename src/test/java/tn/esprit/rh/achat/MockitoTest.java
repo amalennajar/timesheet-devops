164328855bbe3fe963b016324f057efb36b7aa5a
amalnajar <amal.ennajar@esprit.tn>
2023-11-04 20:54:16 +0100

    update code

--- src/test/java/tn/esprit/rh/achat/MockitoTest.java
+++ src/test/java/tn/esprit/rh/achat/MockitoTest.java
@@ -1,95 +1,82 @@
 
 package tn.esprit.rh.achat;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import tn.esprit.rh.achat.controllers.StockRestController;
-import tn.esprit.rh.achat.entities.Stock;
-import tn.esprit.rh.achat.repositories.StockRepository;
-import tn.esprit.rh.achat.services.StockServiceImpl;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tn.esprit.rh.achat.entities.Reglement;
+import tn.esprit.rh.achat.repositories.ReglementRepository;
+import tn.esprit.rh.achat.services.ReglementServiceImpl;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
-import java.util.UUID;
+import java.util.Optional;
 
-import static com.fasterxml.jackson.databind.jsonFormatVisitors.JsonValueFormat.UUID;
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
-import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
 
-
-@RunWith( SpringRunner.class)
-@ContextConfiguration(classes = {StockServiceImpl.class})
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class MockitoTest {
 
-    private StockServiceImpl service;
-    private StockRepository repository;
-//    private MockMvc mockMvc;
-//    ObjectMapper objectMapper = new ObjectMapper();
-//    ObjectWriter objectWriter = objectMapper.writer();
-//@Mock
-// private StockRepository stockRepository;
-//@InjectMocks
-//    private StockRestController stockRestController;
-//
-//    long id1 = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
-//    long id2 = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
-//    long id3 = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
-//    Stock RECORD_1 = new Stock(id1,"alimentaire",50,5);
-//    Stock RECORD_2 = new Stock(id2,"électro",100,20);
-//    Stock RECORD_3 = new Stock(id3,"vestimentaire",150,10);
-//@Before
-//    public void setUp(){
-//    MockitoAnnotations.initMocks(this);
-//    this.mockMvc = MockMvcBuilders.standaloneSetup(stockRestController).build();
-//
-//   }
-//@Test
-////    public void getAllRecords_success()throws Exception{
-//
-//    List<Stock> records = new ArrayList<>(Arrays.asList(RECORD_1,RECORD_2,RECORD_3));
-//    Mockito.when(stockRepository.findAll()).thenReturn(records);
-//    mockMvc.perform(MockMvcRequestBuilders
-//            .get("/retrieve-all-stocks")
-//            .contentType(MediaType.APPLICATION_JSON))
-//            .andExpect(status().isOk())
-//            .andExpect(MockMvcResultMatchers.jsonPath("$",hasSize(3)))
-//            .andExpect((ResultMatcher) jsonPath("$[2].libelleStock", is("alimentaire")));
-//}
+    @MockBean
+    ReglementRepository rep;
+
+    @Autowired
+    ReglementServiceImpl service;
+
 
     @Test
-    public void getStockTest(){
-        System.out.println(" get test stock");
-        long id = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
-        long id2 = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+    @Order(1)
+    void ajoutReglemntTest(){
 
-        repository = mock(StockRepository.class);
-        service = new StockServiceImpl(repository);
-
-        List<Stock> stockList = new ArrayList<>();
-        stockList.add(new Stock(id,"alimentaire",100,5));
-        stockList.add(new Stock(id2,"vest",50,10));
-        when(repository.findAll()).thenReturn(stockList);
-
+        Reglement r = new Reglement() ;
+        r.setDateReglement(new Date());
+        r.setMontantRestant(7);
+        given(this.rep.save(r)).willReturn(r);
+        Reglement r2 = service.addReglement(r);
+        assertTrue(r.getMontantRestant() == r2.getMontantRestant());
     }
 
-}
+    @Test
+    @Order(2)
+    void retrieveReglementTest(){
+        Reglement r = new Reglement() ;
+        r.setIdReglement(1L);
+        r.setDateReglement(new Date());
+        r.setMontantRestant(7);
+        given(this.rep.findById(1L)).willReturn(Optional.of(r));
+        Reglement r2 = service.retrieveReglement(1L);
+        assertTrue(r.getMontantRestant() == r2.getMontantRestant());
+    }
 
+
+    @Test
+    @Order(3)
+    void retrieveAllReglementsTest(){
+        Reglement r = new Reglement() ;
+        r.setDateReglement(new Date());
+        r.setMontantRestant(7);
+
+        Reglement r2 = new Reglement() ;
+        r2.setDateReglement(new Date());
+        r2.setMontantRestant(6);
+
+        List<Reglement> list = new ArrayList<>();
+        list.add(r);
+        list.add(r2);
+
+        given(this.rep.findAll()).willReturn(list);
+
+        List<Reglement> list2  = service.retrieveAllReglements();
+        assertTrue(list2.size() == list.size());
+    }
+
+
+
+}
