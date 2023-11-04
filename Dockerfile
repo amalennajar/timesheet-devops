0a52583b582595d47b4631c75be77b31e579c417
amalnajar <amal.ennajar@esprit.tn>
2023-11-04 23:08:27 +0100

    update code

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,9 @@ RUN apt-get update && apt-get install -y curl
 
 # Download the JAR file from the Nexus repository and name it achat-1.0.jar
 RUN curl -o achat-1.1.jar -L http://192.168.56.5:8081/repository/maven-releases/tn/esprit/rh/achat/1.1/achat-1.1.jar
-
+# Use a build argument to set the password during the build
+ARG DB_PASSWORD
+ENV DB_PASSWORD=$DB_PASSWORD
 # Command to run the Java application when the container starts
 ENTRYPOINT ["java", "-jar", "achat-1.1.jar"]
 
---
+++ src/test/java/tn/esprit/rh/achat/reglementServiceMock.java
@@ -0,0 +1,29 @@
+package tn.esprit.rh.achat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tn.esprit.rh.achat.repositories.ReglementRepository;
+import tn.esprit.rh.achat.services.ReglementServiceImpl;
+
+@ExtendWith(MockitoExtension.class)
+public class reglementServiceMock {
+
+    @Mock
+    private  ReglementRepository reglementRepository ;
+
+    @InjectMocks
+    private ReglementServiceImpl reglementService;
+
+    @Test
+
+
+
+
+
+
+
+
+}
